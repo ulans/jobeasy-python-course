15448d8e083678117266b535483f6f66fa2928d8
ulans <usulaimankul@gmail.com>
2020-04-19 13:17:18 -0500

    Homework #2

--- lesson_2/homework_2_1.py
+++ lesson_2/homework_2_1.py
@@ -1,33 +1,33 @@
 # Create three strings using three different methods. Save your result to result_string_1, result_string_2,
 # result_string_3 variables
 
-result_string_1 = None
-result_string_2 = None
-result_string_3 = None
+result_string_1 = 'Apple'
+result_string_2 = 'Banana'
+result_string_3 = 'Orange'
 
 
 # Enter a string with your name using console and save the result to result_name variable
 
-result_name = None
+result_name = 'Michael Jordan'
 
 
-# Enter your first and  last name. Join them together with a space in
+# Enter your first and last name. Join them together with a space in
 # between. Save a result in a variable result_full_name and
 # save the length of the whole name in result_full_name_length variable.
 
-first_name = None
-last_name = None
-result_full_name = None
-result_full_name_length = None
+first_name = 'Michael'
+last_name = 'Jordan'
+result_full_name = 'Michael Jordan'
+result_full_name_length = result_full_name.count()
 
 
 # Enter the capital city of California State in lower case. Change the case to title case.
 # Save the result in result_ca_capital variable
 
-result_ca_capital = None
+result_ca_capital = 'sacramento'.title()
 
 
 # Enter the name of our planet. Change the case to upper case. Save the result in
 # result_planet variable
 
-result_planet = None
+result_planet = 'Earth'.upper()
